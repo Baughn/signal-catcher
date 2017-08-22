0a08b444d144c60aed337936ed61b36f934f32af
Svein Ove Aas <sveina@gmail.com>
2017-08-22 20:16:48 +0100

    Be firmer about quitting

--- src/main/java/info/brage/minecraft/signalcatcher/SignalCatcher.java
+++ src/main/java/info/brage/minecraft/signalcatcher/SignalCatcher.java
@@ -1,6 +1,7 @@
 package info.brage.minecraft.signalcatcher;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
@@ -11,9 +12,11 @@ import sun.misc.SignalHandler;
 @Mod(modid = SignalCatcher.MODID, version = SignalCatcher.VERSION, acceptedMinecraftVersions = "[1.11.2,)", acceptableRemoteVersions = "*")
 public class SignalCatcher {
     static final String MODID = "signal-catcher";
-    static final String VERSION = "1.0";
+    static final String VERSION = "1.1";
 
     private MinecraftServer server;
+
+    private static int signalCount = 0;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
@@ -32,9 +35,22 @@ public class SignalCatcher {
     private final SignalHandler handler = new SignalHandler() {
         @Override
         public void handle(Signal signal) {
-            System.out.println("Terminating from signal");
-            if (server != null)
-                server.initiateShutdown();
+            if (signalCount == 0) {
+                System.out.println("Terminating from signal");
+                if (server != null) {
+                    server.initiateShutdown();
+                } else {
+                    System.out.println("...but no server found? Dazed, confused and doing nothing.");
+                }
+                System.out.println("A second signal will trigger standard Forge shutdown, a third will trigger abrupt termination.");
+            } else if (signalCount == 1) {
+                System.out.println("Caught second signal, triggering Forge-standard shutdown.");
+                FMLCommonHandler.instance().exitJava(1, false);
+            } else {
+                System.out.println("Caught third signal, triggering abrupt termination. Corruption is possible.");
+                FMLCommonHandler.instance().exitJava(2, true);
+            }
+            signalCount++;
         }
     };
 
